c76d49aeb1f9832d7da4f7d01baf1371b6541af2
Sneary93 <122059496+Sneary93@users.noreply.github.com>
2023-12-02 23:40:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,103 +1,83 @@
 import streamlit as st
-import os
-import openai
-import configparser
-from audiocraft.models import MusicGen, MultiBandDiffusion
-import torch
-import soundfile as sf
-import io
-import pandas as pd
-import numpy as np  # Make sure to import numpy
+import configparser 
+from langchain.document_loaders.csv_loader import CSVLoader
+from audiocraft.models import MusicGen
+from audiocraft.models import MultiBandDiffusion
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-# Configuration Loading
-config_ini_location = 'config_new.ini'
+from google.colab import drive
+drive.mount('/content/drive')
+
+config_ini_location = '/content/drive/MyDrive/Colab Notebooks/IS883/config.ini'
 config = configparser.ConfigParser()
 config.read(config_ini_location)
 openai_api_key = config['OpenAI']['API_KEY']
 
-# Model Initialization
-USE_DIFFUSION_DECODER = False
-model = MusicGen.get_pretrained('facebook/musicgen-small')
-if USE_DIFFUSION_DECODER:
-    mbd = MultiBandDiffusion.get_mbd_musicgen()
+# Change directory to the cloned repository
+%cd ai_bruce
 
+# Pull the latest changes from GitHub (optional, in case changes were made outside Colab)
+!git pull origin main
+
+# Load CSV file
+csv_path = "/content/drive/MyDrive/Colab Notebooks/IS883/IS883 Project_Team1/Sleep_health_and_lifestyle_dataset.csv"
+loader = CSVLoader(csv_path)
+data = loader.load()
+text_data = data[2].page_content
+
+
+
+# Spotify API credentials
+SPOTIPY_CLIENT_ID = '20dbe90b141f4177b3331613a262e24e'
+SPOTIPY_CLIENT_SECRET = '3e164a1c553e4ad3b5e450af22c15462'
+SPOTIPY_REDIRECT_URI = 'http://localhost:3000/callback'
+
+# Initialize Spotify client
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=SPOTIPY_CLIENT_ID,
+    client_secret=SPOTIPY_CLIENT_SECRET,
+    redirect_uri=SPOTIPY_REDIRECT_URI,
+    scope="user-read-private user-read-email",
+))
+
+# Music generation setup
+model = MusicGen.get_pretrained('facebook/musicgen-small')
 model.set_generation_params(
     use_sampling=True,
     top_k=250,
-    duration=5
+    top_p=0.0,
+    temperature=1.0,
+    duration=15.0,
+    cfg_coef=3.0,
 )
 
-def generate_audio(result_text):
-    output = model.generate(
-        descriptions=[result_text],
-        progress=True, return_tokens=True
-    )
-    audio_data = output[0]
-    out_diffusion = None
-    if USE_DIFFUSION_DECODER:
-        out_diffusion = mbd.tokens_to_wav(output[1])
-    return audio_data, out_diffusion
+# Streamlit app
+st.title("Music Generation and Spotify Integration App")
 
-def numpy_to_bytes(audio_data, sample_rate):
-    buffer = io.BytesIO()
-    audio_data_2d = np.squeeze(audio_data)  # Remove singleton dimensions
-    sf.write(buffer, audio_data_2d.T, sample_rate, format='WAV')  # Transpose if necessary
-    return buffer.getvalue()
+# Display CSV data
+st.header("CSV Data")
+st.write(text_data)
 
-# Streamlit App
-def main():
-    st.title("Fitness Activity Audio Generator")
+# Display Spotify playlists
+st.header("Spotify Playlists")
+user_playlists = sp.current_user_playlists()
+for playlist in user_playlists['items']:
+    st.write(playlist['name'])
 
-    # Collecting user inputs
-    st.subheader("Activity Information")
-    activity_date = st.text_input("Activity Date", value='2023-10-29')
-    start_time = st.text_input("Start Time", value='12:00:00')
-    end_time = st.text_input("End Time", value='12:10:00')
-    activity_type = st.text_input("Type", value='Walking')
-    duration = st.text_input("Duration (seconds)", value='600')
-    distance = st.text_input("Distance (meters)", value='300')
-    calories_burned = st.text_input("Calories Burned", value='20')
-    avg_heart_rate = st.text_input("Average Heart Rate", value='80')
-    peak_heart_rate = st.text_input("Peak Heart Rate", value='90')
-    steps = st.text_input("Steps", value='400')
-    notes = st.text_area("Notes", value='I feel tired and unmotivated.')
+# Music generation based on CSV data
+st.header("Generated Music")
+result_text = " ".join(text_data.split()[:50])  # Example: Take the first 50 words from CSV data
+output = model.generate(descriptions=[result_text], progress=True, return_tokens=True)
+st.audio(output[0], format="audio/wav")
 
-    # Action Button
-    if st.button("Action"):
-        user_input = f"""
-        Activity Date: {activity_date}
-        Start Time: {start_time}
-        End Time: {end_time}
-        Type: {activity_type}
-        Duration: {duration}
-        Distance: {distance}
-        Calories Burned: {calories_burned}
-        Average Heart Rate: {avg_heart_rate}
-        Peak Heart Rate: {peak_heart_rate}
-        Steps: {steps}
-        Notes: {notes}
-        """ 
-        
+# Display Spotify user profile
+st.header("Spotify User Profile")
+user_profile = sp.current_user()
+st.write(user_profile)
 
-        # OpenAI API Call
-        openai.api_key = openai_api_key
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=(
-                "Generate a text based on what this person's activity shows. "
-                "If it has a negative implication, suggest a positive statement "
-                "to help and encourage them to recover from the negative situation.\n"
-                f"Response: {user_input}"
-            ),
-            max_tokens=50
-        )
+# After making changes to your Streamlit app, commit and push the changes to GitHub
+!git add app.py
+!git commit -m "Update app.py"
+!git push origin main
 
-        result_text = response.choices[0].text.strip()
-
-        print(result_text +"...................... :)")
-
-        # Generating and displaying audio
-        audio_data, out_diffusion = generate_audio(result_text)
-        st.audio(numpy_to_bytes(audio_data, 32000), format='wav')
-        if out_diffusion:
-            st.audio(numpy_to_bytes(out_diffusion, 32000), format='wav')
